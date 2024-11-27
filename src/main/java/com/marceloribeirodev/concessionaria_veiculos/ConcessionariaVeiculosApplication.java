4771c6b1cb2410dc79bd17684eb3566b9b4dc4c8
unknown <contatoceloribeiro@gmail.com>
2024-11-27 17:54:05 -0300

    Projeto iniciado

---
+++ src/main/java/com/marceloribeirodev/concessionaria_veiculos/ConcessionariaVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.marceloribeirodev.concessionaria_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionariaVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marceloribeirodev/concessionaria_veiculos/ConcessionariaVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloribeirodev.concessionaria_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
